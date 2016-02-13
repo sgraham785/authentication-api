8b71d141f2fa6bae8fbbdda81345a64af67de64b
Sean Graham <admin@example.com>
2016-02-12 17:32:47 -0800

    updated node_modules, fixed package licence item

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ if (fs.existsSync(envFile)){
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
