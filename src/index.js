8b0a45e4734e4e0ac8719183c5f8f1ebfafdc114
Sean Graham <sean.graham@internetbrands.com>
2017-06-19 16:47:45 -0700

    finished initial docker dev setup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@ import fs from 'fs'
 import path from 'path'
 import express from 'express'
 import favicon from 'serve-favicon'
+import session from 'express-session'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import helmet from 'helmet'
+import csurf from 'csurf'
+import logger from 'morgan'
+import https from 'https'
+import { } from 'dotenv'
 import swagger from 'swagger-jsdoc'
 import jwtVerify from './middleware/jsonwebtoken/verify'
 import { authorizeRequest } from './middleware/authorization'
 require('./middleware/logger')
+
+const privateKey = fs.readFileSync('sslcert/server.key', 'utf8')
+const certificate = fs.readFileSync('sslcert/server.crt', 'utf8')
+const credentials = {key: privateKey, cert: certificate}
+
+const host = process.env.SERVER_HOST || '0.0.0.0'
+const port = process.env.SERVER_port || '8443'
+const env = process.env.NODE_ENV || 'development'
 
 /**
  * TODOs:
@@ -14,10 +30,149 @@ require('./middleware/logger')
  * - Set session & JWT to REDIS
  */
 
-export const app = express()
+export let app = express()
 app.disable('x-powered-by')
 
 app.use(favicon(path.resolve(__dirname, 'views/favicon.ico')))
+
+// ======== *** BODY-PARSER MIDDLEWARE ***
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+    type: 'application/x-www-form-urlencoded'
+  })
+)
+app.use(
+  bodyParser.json({
+    type: ['application/json', 'application/vnd.api+json']
+  })
+)
+
+// ======== *** SESSION MIDDLEWARE ***
+// TODO: implement session store:
+// https://www.npmjs.com/package/connect-session-knex
+// https://www.npmjs.com/package/connect-redis
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    // store: ,
+    resave: false,
+    saveUninitialized: true,
+    expires: new Date(Date.now() + 3600000), // 1 Hour
+    cookie: {httpOnly: true, secure: true}
+  })
+)
+
+// ======== *** CORS MIDDLEWARE ***
+// TODO: fix multi origin
+// var corsWhitelist = ['localhost', 'http://example2.com']
+
+// Set CORS
+app.use(
+  cors({
+    origin: host,
+    methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'HEAD'],
+    allowedHeaders: ['Content-type', 'Accept', 'X-Access-Token', 'X-Key'],
+    credentials: true,
+    maxAge: 3600
+  })
+)
+
+// ======== *** SECURITY MIDDLEWARE ***
+app.use(helmet())
+// set CSP
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", 'localhost'],
+      scriptSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        "'unsafe-eval'",
+        'ajax.googleapis.com',
+        'www.google-analytics.com'
+      ],
+      styleSrc: ["'self'", "'unsafe-inline'", 'ajax.googleapis.com'],
+      imgSrc: ["'self'", 'data:'],
+      connectSrc: ["'self'"]
+    },
+    // This module will detect common mistakes in your directives and throw errors
+    // if it finds any. To disable this, enable "loose mode".
+    loose: false,
+
+    // Set to true if you only want browsers to report errors, not block them.
+    // You may also set this to a function(req, res) in order to decide dynamically
+    // whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
+    reportOnly: false,
+
+    // Set to true if you want to blindly set all headers: Content-Security-Policy,
+    // X-WebKit-CSP, and X-Content-Security-Policy.
+    setAllHeaders: false,
+
+    // Set to true if you want to disable CSP on Android where it can be buggy.
+    disableAndroid: false,
+
+    // Set to false if you want to completely disable any user-agent sniffing.
+    // This may make the headers less compatible but it will be much faster.
+    // This defaults to `true`.
+    browserSniff: true
+  })
+)
+
+// ======== *** CSURF MIDDLEWARE ***
+const valueFunction = req => {
+  const result =
+    (req.body && req.body._csrf) ||
+    (req.query && req.query._csrf) ||
+    (req.cookies && req.cookies['XSRF-TOKEN']) ||
+    req.headers['csrf-token'] ||
+    req.headers['xsrf-token'] ||
+    req.headers['x-csrf-token'] ||
+    req.headers['x-xsrf-token']
+
+  return result
+}
+// set CSURF
+app.use(csurf({value: valueFunction}))
+
+app.use((req, res, next) => {
+  res.cookie('XSRF-TOKEN', req.csrfToken())
+  res.locals.csrftoken = req.csrfToken()
+  next()
+})
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (process.env.NODE_ENV === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: err
+    })
+  })
+  app.use(logger('dev'))
+} else {
+  // production error handler
+  // no stacktraces leaked to user
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: {}
+    })
+  })
+  app.use(
+    logger('common', {
+      skip (req, res) {
+        return res.statusCode < 400
+      },
+      stream: path.resolve(__dirname, '/../app_errors.log')
+    })
+  )
+}
 
 // Setup Swagger on Development
 if (process.env.NODE_ENV === 'development') {
@@ -49,12 +204,12 @@ app.get('/', (request, response) => {
 
 app.get('/v1', (request, response) => {
   response.set('Content-Type', 'application/json')
-  response.send(200, 'Here and healthy!')
+  response.status(200).send('Here and healthy!')
 })
 
 process.env.NODE_ENV === 'development'
-  ? app.all('/v1/private/*')
-  : app.all('/v1/private/*', authorizeRequest)
+  ? app.all('/v1/*')
+  : app.all('/v1/*', authorizeRequest)
 
 // Register & use declared resource naming
 const resources = fs.readdirSync(path.join(__dirname, 'resources'))
@@ -70,3 +225,7 @@ app.use((req, res, next) => {
   err.status = 404
   next(err)
 })
+
+app = https.createServer(credentials, app)
+app.listen(port, host)
+console.log('Server running on, %s:%d. NODE_ENV = %s', host, port, env)
