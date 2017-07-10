ccb1fdf057ac306426fdfdf8065fa26d189070da
Sean Graham <sean.graham@internetbrands.com>
2017-07-09 20:00:59 -0700

    working login with jwt redis session

--- src/resource/authentication/controller.js
+++ src/resource/authentication/controller.js
@@ -1,31 +1,47 @@
 import bcrypt from 'bcryptjs-then'
-import Promise from 'bluebird'
-import { Auth, Info } from '../../models/users'
+import { Auth } from '../../models/users'
 
 export const login = (req, res) => {
   if (!req.body.email || !req.body.password) return res.status(400).send({ error: true, data: { message: 'You must provide an email and password' } })
 
   const data = {
-    email: req.body.email,
+    email: req.body.email.toLowerCase().trim(),
     password: req.body.password
   }
 
   Auth.findOne({
     email: data.email
   })
-    .then(user => {
-      console.log(`data.password --> ${data.password}`)
-    console.log(`user.password --> ${user.password}`)  
-    return bcrypt.compare(data.password, user.password)
-      .then(valid => {
-        return res.send(`valid --> ${valid}`)
-      // set session
-      }, err => {
-        console.error(`Login err--> ${JSON.stringify(err)}`)
+  .then(user => {
+    return bcrypt.compare(data.password, user.get('password'))
+    .then(valid => {
+      if (valid) {
+        // Build session data object
+        let profile = {
+          uuid: user.get('uuid')
+        }
+        // this will be stored in redis
+        req.jwtSession.profile = profile
+
+        // this will be attached to the JWT
+        let claims = {
+          iss: process.env.SERVER_HOST,
+          aud: process.env.SERVER_HOST
+        }
+
+        req.jwtSession.create(claims, (err, token) => {
+          if (err) console.error(`Set Token err --> ${err}`)
+          res.status(200).send({ error: false, data: { token: token } })
+        })
+      } else {
         return res.status(422)
-          .send({ error: true, data: { message: 'Profile can not be validated' } })
-      })
-    // return res.status(400).send({ error: false, data: { message: user } })
+          .send({ error: true, data: { message: 'Profile cannot be validated' } })
+      }
+    }, err => {
+      console.error(`Login err--> ${JSON.stringify(err)}`)
+      return res.status(422)
+        .send({ error: true, data: { message: 'Profile can not be validated' } })
+    })
   })
   .catch(Auth.NotFoundError, () => {
     res.status(422)
