76d4e6cdcaea6e5e06738cf69b253ed49a0e08b2
Sean Graham <sean.graham@internetbrands.com>
2017-07-21 08:41:20 -0700

    added schema validation; rm unused model; fixed req session var

--- src/resource/authentication/controller.js
+++ src/resource/authentication/controller.js
@@ -1,71 +1,78 @@
+import Promise from 'bluebird'
+import Joi from 'joi'
 import bcrypt from 'bcryptjs-then'
 import { Auth } from '../../models/users'
+import schema from './schema'
 
+// Make schema validation a Promise
+let validate = Promise.promisify(Joi.validate)
 /**
  * TODOS:
- * - add better schema validation
  */
-export const login = (req, res) => {
+export const login = Promise.method((req, res) => {
   if (!req.body.email || !req.body.password) return res.status(400).send({ error: true, data: { message: 'You must provide an email and password' } })
 
-  const data = {
+  const data = Object.freeze({
     email: req.body.email.toLowerCase().trim(),
     password: req.body.password
-  }
-
-  Auth.findOne({
-    email: data.email
   })
-  .then(user => {
-    return bcrypt.compare(data.password, user.get('password'))
-    .then(valid => {
-      if (valid) {
-        // Build session data object
-        let profile = {
-          uuid: user.get('uuid')
-        }
-        // this will be stored in redis
-        req.jwtSession.profile = profile
 
-        // this will be attached to the JWT
-        let claims = {
-          iss: process.env.SERVER_HOST,
-          aud: process.env.SERVER_HOST
-        }
-
-        req.jwtSession.create(claims, (err, token) => {
-          if (err) console.error(`Set Token err --> ${err}`)
-          res.status(200).send({ error: false, data: { token: token } })
+  validate(data, schema)
+    .then(validated => {
+      return Auth
+        .findOne({
+          email: validated.email
         })
-      } else {
-        return res.status(422)
-          .send({ error: true, data: { message: 'Profile cannot be validated' } })
-      }
+        .then(user => {
+          return bcrypt.compare(validated.password, user.get('password'))
+          .then(valid => {
+            // Build session data object
+            let profile = {
+              uuid: user.get('uuid')
+            }
+            // this will be stored in redis
+            req.session.profile = profile
+
+            // this will be attached to the JWT
+            let claims = {
+              iss: process.env.SERVER_HOST,
+              aud: process.env.SERVER_HOST
+            }
+
+            req.session.create(claims, (err, token) => {
+              if (err) console.error(`Set Token err --> ${err}`)
+              res.status(200).send({ error: false, data: { token: token } })
+            })
+          }, err => {
+            console.error(`Login err--> ${JSON.stringify(err)}`)
+            return res.status(422)
+              .send({ error: true, data: { message: 'Profile can not be validated' } })
+          })
+        })
+        .catch(Auth.NotFoundError, () => {
+          res.status(422)
+            .send({ error: true, data: { message: 'Profile not found' } })
+        })
     }, err => {
-      console.error(`Login err--> ${JSON.stringify(err)}`)
+      console.error(`Registration Validation err--> ${JSON.stringify(err)}`)
       return res.status(422)
-        .send({ error: true, data: { message: 'Profile can not be validated' } })
+        .send({ error: true, data: { message: 'Please fill in all required fields' } })
     })
-  })
-  .catch(Auth.NotFoundError, () => {
-    res.status(422)
-      .send({ error: true, data: { message: 'Profile not found' } })
-  })
-  .catch(err => {
-    console.error(err)
-    res.status(500).send({ error: true, data: { message: err.message } })
-  })
-}
+    .catch(err => {
+      console.error(err)
+      res.status(500).send({ error: true, data: { message: err.message } })
+    })
+})
 
 export const refresh = (req, res) => {
-  req.jwtSession.touch(err => {
+  req.session.touch(err => {
     if (err) console.error(`Session refresh err --> ${err}`)
-    res.status(200).send({ error: false, data: { message: req.jwtSession.toJSON() } })
+    res.status(200).send({ error: false, data: { message: req.session.toJSON() } })
   })
 }
 
 export const logout = (req, res) => {
-  req.jwtSession.destroy(err => {
+  req.session.destroy(err => {
     if (err) console.error(`Session destroy err --> ${err}`)
     res.status(200).send({ error: false, data: { message: 'You\'ve successfully been logged out!' } })
   })
--- src/resource/authentication/schema.js
+++ src/resource/authentication/schema.js
@@ -1,7 +1,16 @@
 import Joi from 'joi'
 
-export default {
+/**
+ * (?=.*[a-z]) must contain at least 1 lowercase alphabetical character
+ * (?=.*[A-Z]) must contain at least 1 uppercase alphabetical character
+ * (?=.*[0-9]) must contain at least 1 numeric character
+ * (?=.*[!@#\$%\^&\*]) contain at least one special character, we are escaping reserved RegEx characters to avoid conflict
+ * (?=.{6,}) must be eight characters or longer
+ */
+let passwordRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})')
+
+export default Joi.object().options({ abortEarly: false }).keys({
   email: Joi.string().email().required(),
-  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+  password: Joi.string().regex(passwordRegex).required(),
   access_token: [Joi.string(), Joi.number()]
-}
+}).without('password', 'access_token')
